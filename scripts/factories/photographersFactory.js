b697f3810af771901eccff83b27aa8ad137cb779
Fons Elodie <elodie.fons66@gmail.com>
2023-10-06 10:55:46 +0200

    Ajout des commentaires

--- scripts/factories/photographersFactory.js
+++ scripts/factories/photographersFactory.js
@@ -1,7 +1,7 @@
-export function articlePhotographer(data) {
+export function articlePhotographer(data) {                             // Fonction Factory qui s'exporte sur le "index.js"
     
-    const { portrait, name, country, city, tagline, price, id } = data;
-    //const createElements
+    const { portrait, name, country, city, tagline, price, id } = data;  // Compilation des données dans un objet
+    //const createElements créer les éléments du Html
     const article = document.createElement("article");
     const linkPhotos = document.createElement("a");
     const imgContainer = document.createElement("div");
@@ -10,25 +10,25 @@ export function articlePhotographer(data) {
     const placeContainer = document.createElement("h3");
     const taglineContainer = document.createElement("h4");
     const priceElement = document.createElement("span");
-    const portraitUrl = `assets/photographers/${portrait}`;
+    const portraitUrl = `assets/photographers/${portrait}`;         // J'ajoute l'URL du portrait de chaque photographes
     
     //URL portrait
-    linkPhotos.href = `photographer.html?id=${id}`;
+    linkPhotos.href = `photographer.html?id=${id}`; //Je créer un lien href pour chaque photographes basé sur l'ID
     
-    //classList / setAttribute
-    imgContainer.classList.add("imgContainer");
+    //classList / setAttribute // J'ajoute des classes et des attributes 
+    imgContainer.classList.add("imgContainer"); 
     img.classList.add("imgPortrait");
     img.setAttribute("src", portraitUrl);
     img.setAttribute("alt", name);
 
-    //textContent
+    //textContent // Contenu des cartes ajoutées grâce aux données récupérées
     nameContainer.textContent = name;
     placeContainer.textContent = `${city}, ${country}`;
     taglineContainer.textContent = tagline;
     priceElement.textContent = `${price}€/jour`;
     
     
-    //appenChild
+    //appenChild // Assigne les Parentés
     imgContainer.appendChild(img);
     linkPhotos.appendChild(imgContainer);
     linkPhotos.appendChild(nameContainer);
@@ -36,6 +36,6 @@ export function articlePhotographer(data) {
     article.appendChild(placeContainer);
     article.appendChild(taglineContainer);
     article.appendChild(priceElement);
-    return article;
+    return article; // Retourne l'article une fois qu'il est construit
   
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,35 @@
+//J'importe la Factory
+
 import { articlePhotographer } from '../factories/photographersFactory.js';
 
+// Je crée une classe qui va fetch les photographes et les afficher sur la page d'accueil
 class photographers {
-    constructor() {
+    constructor() { // initialisation du this.data grace au consturctor
         this.data = [];
     }
  
+    //Fonction asynchrone de fetch et appel de la fonction d'affichage des données
     async getPhotographers() {
         const response = await fetch("/data/photographers.json");
         const res = await response.json();
-        this.data = res.photographers;
+        this.data = res.photographers; // Séparation du JSON pour ne garder que les photographes
         console.log(this.data);
         this.displayData();
        
     }
  
+    //Fonction d'affichage des photographes dans la class photographersSection
     displayData() {
-        const photographers = this.data;
-        const photographersSection = document.querySelector(".photographer_section");
+        const photographers = this.data; // Assignation de la data des photographers
+        const photographersSection = document.querySelector(".photographer_section"); // Appel de la DIV "photographer_section"
  
+        // Affiche les données pour chaque photographes à la suite
         photographers.forEach((photographer) => {
-            const photographerModel = articlePhotographer(photographer);
-            photographersSection.appendChild(photographerModel);
+            const photographerModel = articlePhotographer(photographer); // Appel le code de la Factory pour gérer l'affichage des données
+            photographersSection.appendChild(photographerModel); // Placement des cartes dans la DIV
         });
     }
 }
- 
+ // J'instancie chaque photographes dans la class
 const photographersInfo = new photographers();
-photographersInfo.getPhotographers();
+photographersInfo.getPhotographers(); // Appel de la fonction getPhotographers pour chaque photographes fetch
