c5e36bd69e43be9cd1c34c651db8ddba32081ce0
Fons Elodie <elodie.fons66@gmail.com>
2023-10-13 10:24:26 +0200

    Commentaires du photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -33,32 +33,32 @@ class thePhotographer {
 const photographerInfos = new thePhotographer();
 photographerInfos.getPhotographer(); // Appel de la fonction getPhotographer pour récuperer le photographe
 
-photographerInfos.headerData = function (){
-    const headerInfo = document.querySelector(".photograph-header .infoContainer");
+photographerInfos.headerData = function (){// Function d'affichage du data du photographe
+    const headerPortrait = document.querySelector(".photograph-header .photoContainer"); // Je sélectionne le container du portrait du photographe
 
-    const titlePhotograph = document.createElement("h1");
-    titlePhotograph.textContent = this.photographer.name;
+    const portraitPhotograph = `assets/photographers/${this.photographer.portrait}`; // J'assgine l'URL du portrait
+    const imgPhotograph = document.createElement("img"); // Je crée un élément HTML img
+    imgPhotograph.setAttribute("src", portraitPhotograph); // J'ajoute en source l'URL du portrait
+    imgPhotograph.setAttribute("alt",  `portrait of ${this.photographer.name}`); // Ajout du texte alternatif pour le portrait (accessibilité)
+
+    headerPortrait.appendChild(imgPhotograph); // J'assigne le parent de l'image du photographe au container de la photo
+
+    const headerInfo = document.querySelector(".photograph-header .infoContainer"); // Je sélectionne le container des informations du photographe
+
+    const titlePhotograph = document.createElement("h1"); // Je crée un élément "h1"
+    titlePhotograph.textContent = this.photographer.name; // Je renseigne le nom du photographe dans le "h1"
     console.log(titlePhotograph);
 
-    const locationPhotograph = document.createElement("h2");
-    locationPhotograph.textContent = `${this.photographer.city}, ${this.photographer.country}`;
-    console.log(locationPhotograph);
+    const locationPhotograph = document.createElement("h2"); // Je crée un élément "h2"
+    locationPhotograph.textContent = `${this.photographer.city}, ${this.photographer.country}`;  // J'assigne le texte de la ville et du pays du photographe
 
-    const tagline = document.createElement("span");
-    tagline.textContent = this.photographer.tagline;
+    const tagline = document.createElement("span");  // Je crée un élément span
+    tagline.textContent = this.photographer.tagline; // J'assigne le texte de la tagline au span
     console.log(tagline);
 
-    headerInfo.appendChild(titlePhotograph);
-    headerInfo.appendChild(locationPhotograph);
-    headerInfo.appendChild(tagline);
+    headerInfo.appendChild(titlePhotograph);  // J'assigne le parent du titre au container des infos du photographe
+    headerInfo.appendChild(locationPhotograph); // J'assigne le parent du localisation au container des infos du photographe
+    headerInfo.appendChild(tagline); // J'assigne le parent du tagline au container des infos du photographe
 
-    const headerPortrait = document.querySelector(".photograph-header .photoContainer");
-
-    const portraitPhotograph = `assets/photographers/${this.photographer.portrait}`;
-    const imgPhotograph = document.createElement("img");
-    imgPhotograph.setAttribute("src", portraitPhotograph);
-    imgPhotograph.setAttribute("alt",  `portrait of ${this.photographer.name}`);
-
-    headerPortrait.appendChild(imgPhotograph);
-
+   
 }
