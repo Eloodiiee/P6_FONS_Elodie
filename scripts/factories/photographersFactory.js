06043ebe28cda2cc8ac790d1c8651cb88a39a583
Fons Elodie <elodie.fons66@gmail.com>
2023-10-23 18:35:50 +0200

    Boutons likes et compteur des likes terminés

--- scripts/factories/photographersFactory.js
+++ scripts/factories/photographersFactory.js
@@ -1,118 +1,124 @@
 export function articlePhotographer(data) {                             // Fonction Factory qui s'exporte sur le "index.js"
     
-    const { portrait, name, country, city, tagline, price, id } = data;  // Compilation des données dans un objet
-    //const createElements créer les éléments du Html
-    const article = document.createElement("article");
-    const linkPhotos = document.createElement("a");
-    const imgContainer = document.createElement("div");
-    const img = document.createElement("img");
-    const nameContainer = document.createElement("h2");
-    const placeContainer = document.createElement("h3");
-    const taglineContainer = document.createElement("h4");
-    const priceElement = document.createElement("span");
-    const portraitUrl = `assets/photographers/${portrait}`; // J'ajoute l'URL du portrait de chaque photographes
-    
-    //Lien du phothographe basé sur l'ID
-    linkPhotos.href = `photographer.html?id=${id}`;  //Je créer un lien href pour chaque photographes basé sur l'ID
-    
-    //classList / setAttribute // J'ajoute des classes et des attributes pour les container des portraits et img de portraits
-    imgContainer.classList.add("imgContainer"); 
-    img.classList.add("imgPortrait");
-    img.setAttribute("id", country);
-    img.setAttribute("src", portraitUrl);
-    img.setAttribute("alt", " "/*`portrait of ${name}`*/); // Ajout du texte "portrait of {}" pour éviter la redondance (accessibilité) pour les lecteurs d'écrans
-
-    //textContent // Contenu des cartes ajoutées grâce aux données récupérées
-    nameContainer.textContent = name;
-    placeContainer.textContent = `${city}, ${country}`;
-    taglineContainer.textContent = tagline;
-    priceElement.textContent = `${price}€/jour`;
-    
-    
-    //appenChild // Assigne les Parentés
-    imgContainer.appendChild(img);
-    linkPhotos.appendChild(imgContainer);
-    linkPhotos.appendChild(nameContainer);
-    article.appendChild(linkPhotos);
-    article.appendChild(placeContainer);
-    article.appendChild(taglineContainer);
-    article.appendChild(priceElement);
-    return article; // Retourne l'article une fois qu'il est construit
+  const { portrait, name, country, city, tagline, price, id } = data;  // Compilation des données dans un objet
+  //const createElements créer les éléments du Html
+  const article = document.createElement("article");
+  const linkPhotos = document.createElement("a");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const nameContainer = document.createElement("h2");
+  const placeContainer = document.createElement("h3");
+  const taglineContainer = document.createElement("h4");
+  const priceElement = document.createElement("span");
+  const portraitUrl = `assets/photographers/${portrait}`; // J'ajoute l'URL du portrait de chaque photographes
   
+  //Lien du phothographe basé sur l'ID
+  linkPhotos.href = `photographer.html?id=${id}`;  //Je créer un lien href pour chaque photographes basé sur l'ID
+  
+  //classList / setAttribute // J'ajoute des classes et des attributes pour les container des portraits et img de portraits
+  imgContainer.classList.add("imgContainer"); 
+  img.classList.add("imgPortrait");
+  img.setAttribute("id", country);
+  img.setAttribute("src", portraitUrl);
+  img.setAttribute("alt", " "/*`portrait of ${name}`*/); // Ajout du texte "portrait of {}" pour éviter la redondance (accessibilité) pour les lecteurs d'écrans
+
+  //textContent // Contenu des cartes ajoutées grâce aux données récupérées
+  nameContainer.textContent = name;
+  placeContainer.textContent = `${city}, ${country}`;
+  taglineContainer.textContent = tagline;
+  priceElement.textContent = `${price}€/jour`;
+  
+  
+  //appenChild // Assigne les Parentés
+  imgContainer.appendChild(img);
+  linkPhotos.appendChild(imgContainer);
+  linkPhotos.appendChild(nameContainer);
+  article.appendChild(linkPhotos);
+  article.appendChild(placeContainer);
+  article.appendChild(taglineContainer);
+  article.appendChild(priceElement);
+  return article; // Retourne l'article une fois qu'il est construit
+
 }
 
 export function photographerPage(data){
 
-    const { portrait, name, country, city, tagline } = data; // Compilations des données dans un objet
+  const { portrait, name, country, city, tagline } = data; // Compilations des données dans un objet
 
-    const headerPhotograph = document.querySelector(".photograph-header"); // Je selectionne le header 
-    
-    const headerInfo = document.querySelector(".photograph-header .infoContainer"); // Je sélectionne le container des informations du photographe
+  const headerPhotograph = document.querySelector(".photograph-header"); // Je selectionne le header 
+  
+  const headerInfo = document.querySelector(".photograph-header .infoContainer"); // Je sélectionne le container des informations du photographe
 
-    const titlePhotograph = document.createElement("h1"); // Je crée un élément "h1"
-    titlePhotograph.textContent = name; // Je renseigne le nom du photographe dans le "h1"
+  const titlePhotograph = document.createElement("h1"); // Je crée un élément "h1"
+  titlePhotograph.textContent = name; // Je renseigne le nom du photographe dans le "h1"
 
-    const locationPhotograph = document.createElement("h2"); // Je crée un élément "h2"
-    locationPhotograph.textContent = `${city}, ${country}`;  // J'assigne le texte de la ville et du pays du photographe
+  const locationPhotograph = document.createElement("h2"); // Je crée un élément "h2"
+  locationPhotograph.textContent = `${city}, ${country}`;  // J'assigne le texte de la ville et du pays du photographe
 
-    const taglinePhotograph = document.createElement("span");  // Je crée un élément span
-    taglinePhotograph.textContent = tagline; // J'assigne le texte de la tagline au span
+  const taglinePhotograph = document.createElement("span");  // Je crée un élément span
+  taglinePhotograph.textContent = tagline; // J'assigne le texte de la tagline au span
 
-    headerInfo.appendChild(titlePhotograph);  // J'assigne le parent du titre au container des infos du photographe
-    headerInfo.appendChild(locationPhotograph); // J'assigne le parent du localisation au container des infos du photographe
-    headerInfo.appendChild(taglinePhotograph); // J'assigne le parent du tagline au container des infos du photographe
+  headerInfo.appendChild(titlePhotograph);  // J'assigne le parent du titre au container des infos du photographe
+  headerInfo.appendChild(locationPhotograph); // J'assigne le parent du localisation au container des infos du photographe
+  headerInfo.appendChild(taglinePhotograph); // J'assigne le parent du tagline au container des infos du photographe
 
-    const headerPortrait = document.querySelector(".photograph-header .photoContainer"); // Je sélectionne le container du portrait du photographe
+  const headerPortrait = document.querySelector(".photograph-header .photoContainer"); // Je sélectionne le container du portrait du photographe
 
-    const portraitPhotograph = `assets/photographers/${portrait}`; // J'assgine l'URL du portrait
-    const imgPhotograph = document.createElement("img"); // Je crée un élément HTML img
-    imgPhotograph.setAttribute("src", portraitPhotograph); // J'ajoute en source l'URL du portrait
-    imgPhotograph.setAttribute("alt", " " /*`portrait of ${this.photographer.name}`*/); // Ajout du texte alternatif pour le portrait (accessibilité)
+  const portraitPhotograph = `assets/photographers/${portrait}`; // J'assgine l'URL du portrait
+  const imgPhotograph = document.createElement("img"); // Je crée un élément HTML img
+  imgPhotograph.setAttribute("src", portraitPhotograph); // J'ajoute en source l'URL du portrait
+  imgPhotograph.setAttribute("alt", " " /*`portrait of ${this.photographer.name}`*/); // Ajout du texte alternatif pour le portrait (accessibilité)
 
-    headerPortrait.appendChild(imgPhotograph); // J'assigne le parent de l'image du photographe au container de la photo
-    return headerPhotograph // Retourne le header une fois qu'il est construit
+  headerPortrait.appendChild(imgPhotograph); // J'assigne le parent de l'image du photographe au container de la photo
+  return headerPhotograph // Retourne le header une fois qu'il est construit
 }
 
-export function displayMedia(data){
-    const { title, photographerId, video, image, likes } = data; // Compilations des données dans un objet
+export function displayMedia(data) {
 
-    const mediaContainer = document.getElementById("mediaContainer"); // Je sélectionne le container des medias
-    const mediaBox = document.createElement("div"); // Je crée une DIV 
-    mediaBox.classList.add("mediaBox"); // J'assigne la classe mediaBox a la DIV 
-    const mediaImage = document.createElement("img"); // Je crée un élément HTML img
-    const mediaVideo = document.createElement("video"); // Je crée un élément HTML video
-    const mediaSubBox = document.createElement("div"); // Je crée une DIV 
-    mediaSubBox.classList.add("mediaSubBox"); // J'ajoute la classe mediaSubBox
-    const mediaTitle = document.createElement("span");  // Je créer le span de titre 
-    mediaTitle.classList.add("mediaTitle"); // J'ajoute la classe mediaTitle
-    const mediaLikes = document.createElement("span");  // Je créer le span de like
-    mediaLikes.classList.add("mediaLikes"); // J'ajoute la classe mediaLikes
-
-    if(video){ // je teste si le media est une video ou une image
-        const mediaUrl = `assets/medias/${photographerId}/${video}`; // J'assigne l'URL a une const
-        mediaVideo.setAttribute("src",mediaUrl); // J'assigne l'URL a la video
-        mediaVideo.setAttribute("alt",title); // J'assigne le alt 
-        mediaVideo.setAttribute("controls",true); //Attribut qui permet de jouer la video, de maniere provisoire tant qu'il n'y a pas la lightbox
-        mediaImage.classList.remove("mediaImage"); // J'enlève la classe mediaImage, pour éviter d'avoir les deux classes
-        mediaVideo.classList.add("mediaVideo"); // J'ajoute la classe mediaVideo
-        mediaBox.appendChild(mediaVideo); // J'assigne le parent de mediaBox sur mediaVideo
-    }
-    else{
-        const mediaUrl = `assets/medias/${photographerId}/${image}`;// J'assigne l'URL a une const
-        mediaImage.setAttribute("src",mediaUrl);// J'assigne l'URL a l'image
-        mediaImage.setAttribute("alt",title);// J'assigne le alt 
-        mediaVideo.classList.remove("mediaVideo");// J'enlève la classe mediaVideo, pour éviter d'avoir les deux classes
+  for (let i = 0; i < data.length; i++) {
+    const mediaContainer = document.getElementById("mediaContainer")// Je sélectionne le container des medias
+    const mediaBox = document.createElement("div"); // Je crée une DIV
+    mediaBox.classList.add("mediaBox"); // J'ajoute la classe mediaBox à la DIV
+    const mediaImage = document.createElement("img");// Je crée un élément HTML img
+    const mediaVideo = document.createElement("video") // Je crée un élément HTML video
+    const mediaSubBox = document.createElement("div"); // Je crée une DIV
+    mediaSubBox.classList.add("mediaSubBox") // J'ajoute la classe mediaSubBox
+    const mediaTitle = document.createElement("span");  // Je crée le span de titre 
+    mediaTitle.classList.add("mediaTitle");// J'ajoute la classe mediaTitle
+    const mediaLikes = document.createElement("span");// Je crée le span de like
+    mediaLikes.classList.add("mediaLikes");// J'ajoute la classe mediaLikes
+    const mediaLikesValue = document.createElement("span");// Je crée le span pour le total de like de la photo
+    mediaLikesValue.classList.add("likeValue");// J'ajoute une classe "likeValue"
+    const mediaLikesHeart = document.createElement("i");// Je crée un élément "i"
+    mediaLikesHeart.classList.add("fa-regular");// J'ajoute la classe "fa-regular"
+    mediaLikesHeart.classList.add("fa-heart");// J'ajoute la classe "fa-heart"
+    mediaLikesHeart.classList.add("likeBtn");// J'ajoute la classe "likeBtn" qui me permettra de selectionner les coeurs comme bouton
+      if (data[i].video) { // je teste si le media est une video ou une image
+      const mediaUrl = `assets/medias/${data[i].photographerId}/${data[i].video}`; // J'assigne l'URL a une const
+      mediaVideo.setAttribute("src", mediaUrl); // J'assigne l'URL a la video
+      mediaVideo.setAttribute("alt", data[i].title);// J'assigne le alt 
+      mediaVideo.setAttribute("id", data[i].id);// J'assigne l'ID
+      mediaVideo.setAttribute("controls", true);// Attribut qui permet de jouer la video, de maniere provisoire tant qu'il n'y a pas la lightbox
+      mediaImage.classList.remove("mediaImage"); // J'enlève la classe mediaImage, pour éviter d'avoir les deux classes
+      mediaVideo.classList.add("mediaVideo"); // J'ajoute la classe mediaVideo
+      mediaBox.appendChild(mediaVideo); // J'assigne le parent de mediaBox sur mediaVideo
+      }
+      else {
+        const mediaUrl = `assets/medias/${data[i].photographerId}/${data[i].image}`;// J'assigne l'URL a une const
+        mediaImage.setAttribute("src", mediaUrl);// J'assigne l'URL a l'image
+        mediaImage.setAttribute("alt", data[i].title);// J'assigne le alt
+        mediaImage.setAttribute("id", data[i].id);// J'assigne l'ID
+        mediaVideo.classList.remove("mediaVideo");;// J'enlève la classe mediaVideo, pour éviter d'avoir les deux classes
         mediaImage.classList.add("mediaImage");// J'ajoute la classe mediaImage
         mediaBox.appendChild(mediaImage);// J'assigne le parent de mediaBox sur mediaImage
     }
-
-    mediaTitle.innerHTML = `${title}`; // Je rempli le contenu du titre
-    mediaLikes.innerHTML = `${likes} <i class="fa-regular fa-heart clickable"></i>`; // Je rempli le contenu des likes
+    mediaTitle.innerHTML = `${data[i].title}`;// Je rempli le contenu du titre
+    mediaLikesValue.innerHTML = `${data[i].likes} `;// Je rempli le contenu de la valeur des likes
+    mediaLikes.appendChild(mediaLikesValue);// J'assigne le parent de mediaLikes a mediaLikesValue
+    mediaLikes.appendChild(mediaLikesHeart);// J'assigne le parent de mediaLikes a mediaLikesHeart
+    mediaContainer.appendChild(mediaBox);;// J'assigne le parent de mediaContainer a mediaBox
     mediaBox.appendChild(mediaSubBox);// J'assigne le parent de mediaBox a mediaSubBox
     mediaSubBox.appendChild(mediaTitle);// J'assigne le parent de mediaSubBox a mediaTitle
     mediaSubBox.appendChild(mediaLikes);// J'assigne le parent de mediaSubBox a mediaLikes
-
-    mediaContainer.appendChild(mediaBox);// J'assigne le parent de mediaContainer a mediaBox
-
+  };
 }
-
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,13 +1,12 @@
 import { photographerPage } from "../factories/photographersFactory.js"; // Importation de la factory pour l'affichage des infos du photographe
-import { displayMedia } from "../factories/photographersFactory.js";  // Importation de la factory pour l'affichage des medias
-
+import { displayMedia } from "../factories/photographersFactory.js";
 // Je crée une classe qui va fetch les photographes en ne gardant que le photographe selectionné
 class thePhotographer { 
     constructor() {
         this.data = []; // initialisation du this.data grace au constructor
         this.photographer = null; //Initialisation du this.photographer grace au constructor
-        this.media = []; // Initialisation du this.media grace au constructor
         this.dataMedia = [] ; // Initialisation du this.dataMedia grace au constructor
+        this.media = []; // Initialisation du this.media grace au constructor
         this.likes = 0; //  Initialisation du this.likes grace au constructor
     }
 
@@ -16,34 +15,34 @@ class thePhotographer {
     async getPhotographer() {
         const currentUrl = (new URL(document.location)).searchParams ; // Permet de récupérer les informations dans l'URL
         const _id = currentUrl.get('id'); // Récupération de l'ID dans l'URL
-        console.log(_id); // Affichage de l'ID grâce a l'URL
+        console.log("Photographer ID : ", _id); // Affichage de l'ID grâce a l'URL
         const response = await fetch(`/data/photographers.json`)  
         const res = await response.json(); 
         this.data = res.photographers; // Séparation du JSON pour ne garder que les photographes
-        console.log(this.data); // Affichage du photographes
+        console.log("Photographers Array : ", this.data); // Affichage du photographes
         this.dataMedia = res.media; // Séparation du JSON pour ne garder que les medias
-        console.log(this.dataMedia); // Affichage de tout les medias
+        console.log("Medias Array : ", this.dataMedia); // Affichage de tout les medias
         
 
         this.data.forEach((photographerData) => { // Boucle forEach qui permet de naviguer dans l'array des photographes
             if(photographerData.id == _id){ // Vérifications du photographe grâce à son ID
                 photographerInfos.photographer = photographerData; //Récupération du photographe recherché
-                console.log(photographerData); // Affichage du photographe de la page actuelle 
+                console.log("Photographer's Data : ", photographerData); // Affichage du photographe de la page actuelle 
                 this.dataMedia.forEach((media) => { //  Boucle forEach qui permet de naviguer dans l'array des medias du photographe
                     if(media.photographerId == _id){ // Vérifications des medias grâce à l'ID du photographe
+                        this.media.push(media);// J'ajoute le contenu des medias dans this.media
                         this.likes += media.likes; // Somme de tout les likes du photographe
-                        this.media = media; // Je récupère les informations des media
-                        console.log(this.media); // je les affiche en log
-                        photographerInfos.displayMedia(); // J'appele la function de la factory displayMedia
                     }
                 });
-                console.log(this.likes); // Affichage de la somme des likes
+                console.log("Photographer's Medias : ", this.media);// Affichage des medias en log
+                console.log("Photographer's total likes : ", this.likes); // Affichage de la somme des likes
                 
             }
         }) 
         photographerInfos.headerData(); // Appel de la fonction headerData() à partir des infos du photographe
-        photographerInfos.bottomRightContainer(); // Appel de la fonction bottomRightContainer() à partir des infos du photographe et de ses medias (like bas de page)
-        photographerInfos.updateLikes();
+        photographerInfos.displayMedia();// Appel de la function displayMedia(), à partir de la fatory
+        photographerInfos.bottomRightContainer(); // Appel de la fonction bottomRightContainer() à partir des infos du photographe et de ses medias
+        photographerInfos.updateLikes();// Appel de la function updateLikes()
     }   
 }
 //J'instencie le photographe dans la classe "thePhotographer"
@@ -61,44 +60,56 @@ photographerInfos.bottomRightContainer = function () { // Function de l'affichag
 
     const likesSpan = document.createElement("span"); // Je crée un span  pour les likes totaux en bas de la page
     likesSpan.classList.add("likes"); // J'ajoute la classe likes
-    likesSpan.innerHTML = `${this.likes} <i class="fa-solid fa-heart"></i>`; // J'assigne le texte des likes 
+    const likesSpanValue =  document.createElement("span"); // Je crée un span  pour la valeur des likes totaux en bas de la page
+    likesSpanValue.classList.add("likesSpanValue");// J'ajoute la classe "likesSpanValue"
+    likesSpanValue.innerHTML = `${this.likes} `; // J'assigne le texte des likes 
+    const likesHeart = document.createElement("i");// Je crée un élément "i"
+    likesHeart.classList.add("fa-solid")// J'ajoute la classe "fa-solid"
+    likesHeart.classList.add("fa-heart")// J'ajoute la classe "fa-heart"
     const pricePerDaySpan = document.createElement("span"); // Je crée un span pour le prix par jour en bas de la page
     pricePerDaySpan.classList.add("priceDay"); // J'ajoute la classe priceDay
-    pricePerDaySpan.innerHTML = `${this.photographer.price} $ / jour`; // J'assigne le texte du prix du photographe
+    pricePerDaySpan.innerHTML = `${this.photographer.price}$ / jour`; // J'assigne le texte du prix du photographe
 
+    likesSpan.appendChild(likesSpanValue); // J'assigne le parent de likesSpan a likesSpanValue
+    likesSpan.appendChild(likesHeart) // J'assigne le parent de likesSpan a likesHeart
     bottomRightContainer.appendChild(likesSpan); // J'assigne le parent du span des likes au container du bas de page
     bottomRightContainer.appendChild(pricePerDaySpan); // J'assigne le parent du prix par jour au container du bas de page
 
     mainSection.appendChild(bottomRightContainer) // J'assigne le parent du container du bas de page au main 
 }
-photographerInfos.displayMedia = function () { // Function d'affichage des medias
-   displayMedia(this.media);// Appel de la factory pour l'affichage des medias
+
+photographerInfos.displayMedia = function () { // Appel de la fonction displayMedia 
+    displayMedia(this.media) // Appel de la fatory displayMedia
 }
-photographerInfos.updateLikes = function () {
-    const likeBtn = document.querySelectorAll(".clickable");
-    let liked = false;
-    console.log(likeBtn);
-    likeBtn.forEach((like) => {
-        like.addEventListener("click", () => {
-            if(liked == false){
-                like.classList.remove("fa-regular");
-                like.classList.add("fa-solid");
-                liked = true;
-                this.likes = this.likes+1;
-                this.media.likes = this.media.likes+1;
-                console.log(this.likes);
-                console.log(this.media.likes);
-            }
-            else{
-                like.classList.remove("fa-solid");
-                like.classList.add("fa-regular");
-                liked = false;
-                this.likes = this.likes-1;
-                this.media.likes = this.media.likes-1;
-                console.log(this.likes);
-                console.log(this.media.likes);
+
+photographerInfos.updateLikes = function () { // Appel de la fonction updateLikes 
+    const likeBtn = document.querySelectorAll(".likeBtn"); // Je selectionne les boutons des coeurs pour les likes
+    const likeBottomRight = document.querySelector(".likesSpanValue"); // Je selectionne la valeur des likes totaux en bas a droite 
+    const likeValue = document.getElementsByClassName("likeValue"); // Je selectionne la valeur des coeurs des medias 
+    for(let i = 0; i < likeBtn.length; i++){ // J'assigne un chiffre selecteur pour les boutons like à l'aide de la boucle for
+        likeBtn[i].addEventListener("click", () => { // J'assigne un événement au click du bouton like
+            if(likeBtn[i].classList.contains("liked") === false){ // Si le bouton like ne contient pas la classe liked, alors 
+                likeBtn[i].classList.remove("fa-regular"); // je retire la classe "fa-regular"
+                likeBtn[i].classList.add("fa-solid"); // j'ajoute la classe "fa-solid"
+                likeBtn[i].classList.add("liked"); // J'ajoute la classe liked
+                this.media[i].likes++; // J'ajoute un like sur le media concerné
+                this.likes++; // J'ajoute un like sur le total de like
+                console.log("Liké ! ", this.media[i].likes); // console log pour montrer que ça a liké
+                likeValue[i].innerHTML = `${this.media[i].likes} `; // J'affiche le like du media concerné
+                likeBottomRight.innerHTML = `${this.likes} `; // J'affiche le total des likes des medias en bas de la page
             }
         })
-            console.log("click");   
-    }) 
+        likeBtn[i].addEventListener("dblclick", () =>{ //// J'assigne un événement au double click du bouton like
+            if(likeBtn[i].classList.contains("liked") === true){ // Si le bouton like  contient  la classe liked, alors
+                likeBtn[i].classList.remove("fa-solid"); // je retire la classe "fa-solid"
+                likeBtn[i].classList.remove("liked"); // J'enleve la classe liked
+                likeBtn[i].classList.add("fa-regular"); // j'ajoute la classe "fa-regular"
+                this.media[i].likes--; // Je retire un like sur le media concerné
+                this.likes--; // Je retire un like sur le total de like
+                console.log("Like enlevé ! ", this.media[i].likes); // console log pour montrer que ça a enlevé le like
+                likeValue[i].innerHTML = `${this.media[i].likes} `; // J'affiche le like du media concerné
+                likeBottomRight.innerHTML = `${this.likes} `; //  J'affiche le total des likes des medias en bas de la page
+            }
+        })
+    }
 }
